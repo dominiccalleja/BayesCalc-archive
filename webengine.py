90bd5fdf1f24712d8ad62dc6149cd531f0b04c26
Nick <nickgray@liverpool.ac.uk>
2021-09-28 19:47:27 +0100

    webengine

--- webengine.py
+++ webengine.py
@@ -5,15 +5,58 @@ app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
-symptoms = ['headache','temperature','cough']
-class Questionare(Resource):
+from engine import *
+Q = Questionaire()
+Q._verbose = False
+Q.generate_questionaire()
+    
+# class Questions(Resource):
+#     def post(self):
+#         json_data = request.get_json()
+#         i = json_data['i']
+        
+#         return {'symptom': symptoms[i],'i': i+1}
+class GetFirstSymptom(Resource):
+    def post(self):
+        print(Q.get_next_symptom())
+        return {'symptom': Q.get_next_symptom()}
+    
+class Yes(Resource):
     def post(self):
         json_data = request.get_json()
-        i = json_data['i']
+        # ppv = json_data['ppv']
+        Q.answer_next_question(1)
+        return {
+            'ppv': str(Q._increment_PPV),
+            'symptom': Q.get_next_symptom()
+        }
         
-        return {'symptom': symptoms[i],'i': i+1}
-    
-api.add_resource(Questionare, '/Questionare')
+class No(Resource):
+    def post(self):
+        json_data = request.get_json()
+        # ppv = json_data['ppv']
+        Q.answer_next_question(1)
+        return {
+            'ppv': str(Q._increment_PPV),
+            'symptom': Q.get_next_symptom()
+        }
+        
+class Dunno(Resource):
+    def post(self):
+        json_data = request.get_json()
+        # ppv = json_data['ppv']
+        Q.answer_next_question(1)
+        return {
+            'ppv': str(Q._increment_PPV),
+            'symptom': Q.get_next_symptom()
+        }
+        
+
+api.add_resource(Yes, '/Yes')
+api.add_resource(No, '/No')
+api.add_resource(Dunno, '/Dunno')
+
+api.add_resource(GetFirstSymptom,"/GetFirstSymptom")
 
 if __name__ == '__main__':
     app.run(port = 5000, debug=False)
