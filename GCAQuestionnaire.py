19777a6964c6cd5a4e4225ab82b1b4ca6b32ac8e
Alexander Wimbush <a.wimbush@liverpool.ac.uk>
2021-09-28 16:28:01 +0100

    added sens and spec for test

--- GCAQuestionnaire.py
+++ GCAQuestionnaire.py
@@ -1,9 +1,11 @@
 import sys
 from engine import Questionaire as Questionaire
 
-print(sys.argv[1])
 Q = Questionaire()
 Q.prevelence = 0.01
 Q.load_questionaire_csv('test_3_inputs.csv')
 Q.generate_questionaire()
 Q.evaluate_questionaire(inputs = [int(i) for i in list(sys.argv[1])])
+
+if len(sys.argv)>=2:
+    Q.what_if_test(float(sys.argv[2]), float(sys.argv[3]))
